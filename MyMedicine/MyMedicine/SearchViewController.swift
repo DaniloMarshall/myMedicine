e07a617ab290a6964d54dc619a0cc2fc5a6a68d5
FernandoHMBastos <fernando.hm.bastos@gmail.com>
2015-08-25 13:01:52 -0300

    search 0.1

--- MyMedicine/MyMedicine/SearchViewController.swift
+++ MyMedicine/MyMedicine/SearchViewController.swift
@@ -8,10 +8,15 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
-
+class SearchViewController: UIViewController, UISearchBarDelegate {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var searchType: UISegmentedControl!
+    @IBOutlet weak var resultsTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.delegate = self
 
         // Do any additional setup after loading the view.
     }
@@ -20,6 +25,73 @@ class SearchViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+    
+        searchBar.showsCancelButton = true
+        tableView.reloadData()
+
+    
+    }
+    
+    func searchBarTextDidEndEditing(searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        
+        // Dismiss the keyboard
+        searchBar.resignFirstResponder()
+        
+    }
+    
+    // Cancel buttom behaviour
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        
+        // Clear any search criteria
+        searchBar.text = ""
+        
+        // Dismiss the keyboard
+        searchBar.resignFirstResponder()
+        
+        //recarrega animações
+        
+        searchBar.showsCancelButton = false
+        
+    }
+    
+    //retornos para a tableview da busca
+//    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        
+//        if(searchList == nil) {
+//            return 0
+//        
+//        return searchList.count
+//    }
+//    
+//    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+//        return 1
+//        
+//    }
+//    
+//    //cria as células-resultado na tabela de busca
+//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        
+//        var cell =  tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! SearchTableViewCell
+//        
+//        var obj: PFObject = searchList.objectAtIndex(indexPath.row) as! PFObject
+//        
+//        cell.searchLabel.text = (obj["name"] as! String)
+//        
+//        return cell
+//    }
+
+
+
     
 
     /*
