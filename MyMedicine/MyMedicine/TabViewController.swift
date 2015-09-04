078f0f6d28d99c3f552aea8d43f55f5e87e26fed
FernandoHMBastos <fernando.hm.bastos@gmail.com>
2015-09-03 22:19:27 -0300

    icones

--- MyMedicine/MyMedicine/TabViewController.swift
+++ MyMedicine/MyMedicine/TabViewController.swift
@@ -16,6 +16,8 @@ class TabViewController: UITabBarController, UITabBarControllerDelegate {
         self.delegate = self
         
         self.navigationController!.navigationBar.tintColor = UIColor(red: 0.16, green: 0.68, blue: 0.62, alpha: 1.0)
+        self.tabBar.tintColor = UIColor(red: 0.16, green: 0.68, blue: 0.62, alpha: 1.0)
+
 
         
 
